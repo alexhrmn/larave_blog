38e30d90e5ae546d3a75580387b2e3c07218d32a
alexhrmn <alexhirman1993@gmail.com>
2021-09-19 21:42:40 +0300

    Initial commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+        <!doctype html>
+
+
+        <title>My blog</title>
+
+
+        <link rel="stylesheet" href="/app.css">
+
+        <body>
+              <article>
+
+                  <?= $post; ?>
+
+              </article>
+
+                <a href="/">Go back</a>
+        </body>
---
+++ routes/web.php
@@ -0,0 +1,55 @@
+    <?php
+
+    use Illuminate\Support\Facades\Route;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
+
+    Route::get('/', function () {
+        return view('posts');
+    });
+
+
+    Route::get('post', function () {
+        return view('post', [
+           'post' => '<h1>Hello world</h1>'
+        ]);
+    });
+
+//    Route::get('/post', function (){
+//        return view ('post', [
+//           'post' => file_get_contents(__DIR__ . '/../resources/posts/my-first-post.html')
+//        ]);
+//    });
+
+//
+//    Route::get('post/{post}', function ($slug) {
+//
+//        $path = __DIR__ . "/../resources/posts/$slug.html";
+//
+//        if(!file_exists($path)) {
+//            return redirect('/');
+//        }
+//
+//
+//        $post = file_get_contents($path);
+//
+//        return view('post', [
+//
+//            'post' => $post
+//        ]);
+//    });
+
+
+
+
+
+
