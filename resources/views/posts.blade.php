b80e18aff1806b6a70283d66a29d65ea70c9639f
alexhrmn <alexhirman1993@gmail.com>
2021-09-26 23:08:34 +0300

    26/09/21 (1)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,41 +6,14 @@
             <link rel="stylesheet" href="/app.css">
 
             <body>
-                <article>
-                    <h1><a href="/posts/my-first-post"> My first Post</a></h1>
-                        <p>1 LAS VEGAS (AP) — USA Basketball didn’t blow up Kevin Durant’s phone with repeated calls to beg and plead for him to play in the Tokyo Olympics. It was a much more dignified process: They asked, then they waited.
 
-                            It was the right move.
-
-                            “They helped me make the decision by letting me be me,” Durant said.
-
-                            Already an owner of two Olympic gold medals, Durant ultimately chose to try for three. The Brooklyn Nets forward will lead the U.S. Olympic team into Tokyo this month and could become just the second men’s player in USA Basketball history to win three golds, joining Carmelo Anthony.
-                        </p>
-                </article>
+               <?php foreach ($posts as $post) : ?>
 
                 <article>
-                    <h1><a href="/posts/my-second-post"> My second Post</a></h1>
-                    <p>2 LAS VEGAS (AP) — USA Basketball didn’t blow up Kevin Durant’s phone with repeated calls to beg and plead for him to play in the Tokyo Olympics. It was a much more dignified process: They asked, then they waited.
 
-                        It was the right move.
-
-                        “They helped me make the decision by letting me be me,” Durant said.
-
-                        Already an owner of two Olympic gold medals, Durant ultimately chose to try for three. The Brooklyn Nets forward will lead the U.S. Olympic team into Tokyo this month and could become just the second men’s player in USA Basketball history to win three golds, joining Carmelo Anthony.
-                    </p>
+                    <?= $post; ?>
 
                 </article>
 
-                <article>
-                    <h1><a href="/posts/my-third-post"> My third Post</a></h1>
-                    <p>3 LAS VEGAS (AP) — USA Basketball didn’t blow up Kevin Durant’s phone with repeated calls to beg and plead for him to play in the Tokyo Olympics. It was a much more dignified process: They asked, then they waited.
-
-                        It was the right move.
-
-                        “They helped me make the decision by letting me be me,” Durant said.
-
-                        Already an owner of two Olympic gold medals, Durant ultimately chose to try for three. The Brooklyn Nets forward will lead the U.S. Olympic team into Tokyo this month and could become just the second men’s player in USA Basketball history to win three golds, joining Carmelo Anthony.
-                    </p>
-                </article>
-
+               <?php endforeach; ?>
             </body>
--- routes/web.php
+++ routes/web.php
@@ -15,33 +15,17 @@
     */
 
     Route::get('/', function () {
-        return view('posts');
+        return view('posts', [
+            'posts' => Post::all()
+        ]);
     });
 
 
     Route::get('posts/{post}', function ($slug) {
 
-        $post = Post::find($slug);
-
         return view('post', [
-           'post' => $post
+           'post' => Post::find($slug)
         ]);
-
-//        if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
-//            return redirect('/');
-//        }
-//
-//        $post = cache()->remember("posts.{$slug}", 1200 , function () use ($path){
-//
-//            var_dump('file_get_contents');
-//
-//            return file_get_contents($path);
-//
-//        });
-//
-//            return view('post', ['post' => $post]);
-
-
     })->where('post', '[A-z_\-]+');
 
 
