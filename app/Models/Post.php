d04d6a08aaef2112fa5eeb3201683608edc1a901
alexhrmn <alexhirman1993@gmail.com>
2021-11-13 02:31:30 +0200

    save 1/11/21 everything good

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -33,15 +33,19 @@
         public static function all()
 
         {
-            return collect(File::files(resource_path("posts")))
-                ->map(fn($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn($document) => new Post(
-                    $document ->title,
-                    $document ->excerpt,
-                    $document ->date,
-                    $document ->body(),
-                    $document ->slug
-                ));
+            return cache()->rememberForever('post.all', function () {
+                return collect(File::files(resource_path("posts")))
+                    ->map(fn($file) => YamlFrontMatter::parseFile($file))
+                    ->map(fn($document) => new Post(
+                        $document ->title,
+                        $document ->excerpt,
+                        $document ->date,
+                        $document ->body(),
+                        $document ->slug
+                    ))
+                    ->sortByDesc('date');
+            });
+
 
         }
 
