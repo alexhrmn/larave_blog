ce2e8dda20182db7c1e36379f7c14f7554c5eef5
alexhrmn <alexhirman1993@gmail.com>
2022-01-09 16:31:37 +0200

    eloquent

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->boolean('is_admin')->default(false);
+            $table->timestamp('email_verified_at')->nullabel();
             $table->rememberToken();
             $table->timestamps();
         });
